1e7b4db77e45fe9fcee59f580a7441852bb088e2
DirektPetr <direktpetr@ya.ru>
2024-07-14 22:06:10 +0300

    16_ compare_each_pair_integers_7_kyu

---
+++ 16_ compare_each_pair_integers_7_kyu.js
@@ -0,0 +1,24 @@
+//? In this kata the aim is to compare each pair of integers from two arrays, and return a new array of large numbers. Note: Both arrays have the same dimensions.
+
+// Example:
+// let arr1 = [13, 64, 15, 17, 88];
+// let arr2 = [23, 14, 53, 17, 80];
+// getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+
+
+function getLargerNumbers(a, b) {
+  let arr = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      arr.push(a[i]);
+    } else {
+      arr.push(b[i]);
+    }
+  }
+  return arr;
+}
+
+let a = [13, 64, 15, 17, 88];
+let b = [23, 14, 53, 17, 80];
+
+console.log(getLargerNumbers(a, b)); //  [23, 64, 53, 17, 88]
